f65e8d4d1250906025bdf7adc6a3a65978727c2c
Aguziendu Ugochukwu <uaguziendu@gmail.com>
2021-05-24 12:34:09 +0100

    clock in and clock out modal

---
+++ src/screens/activity/data.tsx
@@ -0,0 +1,41 @@
+function createData(date: Date, clockin: Date, clockout: Date) {
+  return { date, clockin, clockout };
+}
+
+export const rows = [
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+  createData(
+    new Date("2020-02-14T23:00:00.000Z"),
+    new Date("2020-02-14T23:09:00.000Z"),
+    new Date("2020-02-14T21:35:00.000Z")
+  ),
+];
